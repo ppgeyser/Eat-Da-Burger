6ad3e1a46318463aa39387866c890823746058ef
Peter Geyser <peterpgeyser@gmail.com>
2019-09-27 13:48:44 -0700

    config setup complete

---
+++ config/orm.js
@@ -0,0 +1,45 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function (table, cb) {
+        var queryString = "SELECT * FROM ??";
+
+        console.log(queryString);
+
+        connection.query(queryString, [table], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    insertOne: function (table, col, val, cb) {
+        var queryString = "INSERT INTO ?? (??) VALUES (?)";
+
+        console.log(queryString);
+
+        connection.query(queryString, [table, col, val], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    updateOne: function (table, col, val, cb) {
+        var queryString = "UPDATE ?? SET ?? = ? WHERE id = ?";
+
+        console.log(queryString);
+
+        connection.query(queryString, [table, col, val], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
+    }
+};
+
+module.exports = orm;
